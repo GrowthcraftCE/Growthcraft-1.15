34e526d3fe488220cc958a43a58671c60a7ba3e4
Alatyami <alatyami@gmail.com>
2020-05-11 07:14:32 -0400

    Resolves #30 Implemented Apple Wood Stairs
    Added Apple Wood Stairs block/itemblock, recipe, loot_table, recipe advancement, lang, and assets.
    
    Deprecated the legacy registerBlocks(...) method in favor of the newer deferredregistries.
    
    Added runtime jei support. It helps to be able to confirm that the recipes are loaded correctly. JEI is preferred over the vanilla recipe book.
    
    Fixed some minor sonarqube code quality checks.

--- src/main/java/growthcraft/apples/init/GrowthcraftApplesBlocks.java
+++ src/main/java/growthcraft/apples/init/GrowthcraftApplesBlocks.java
@@ -3,8 +3,10 @@ package growthcraft.apples.init;
 import growthcraft.Growthcraft;
 import growthcraft.apples.common.block.BlockAppleButton;
 import growthcraft.apples.common.block.BlockApplePlanks;
+import growthcraft.apples.common.block.BlockAppleStairs;
 import growthcraft.apples.shared.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -19,7 +21,7 @@ public class GrowthcraftApplesBlocks {
     // public static Block definitions.
     public static final RegistryObject<BlockAppleButton> appleButton;
     public static final RegistryObject<BlockApplePlanks> applePlank;
-    // TODO: #30 public static final RegistryObject<BlockAppleStairs> appleStairs;
+    public static final RegistryObject<BlockAppleStairs> appleStairs;
 
     // Static initializer for Growthcraft Apples blocks.
     static {
@@ -29,14 +31,22 @@ public class GrowthcraftApplesBlocks {
         applePlank = BLOCKS.register(
                 "apple_plank",
                 () -> new BlockApplePlanks("apple_plank"));
+        appleStairs = BLOCKS.register(
+                "apple_stairs",
+                () -> new BlockAppleStairs(Blocks.OAK_PLANKS, "apple_stairs"));
     }
 
     private GrowthcraftApplesBlocks() { /* Prevent Default Public Constructor */ }
 
-    // Add the blocks to the registry.
+    /**
+     * Legacy block registration. Use DeferredRegister<Block> and RegistryObject<T> instead.
+     *
+     * @param registry Forge Block Registry
+     * @deprecated use GrowthcraftBambooBlocks.BLOCKS deferred registry instead.
+     */
+    @Deprecated
     public static void registerBlocks(IForgeRegistry<Block> registry) {
         Growthcraft.LOGGER.debug("Growthcraft Apples Registering blocks ...");
-
     }
 
     /**
@@ -51,8 +61,10 @@ public class GrowthcraftApplesBlocks {
         BLOCKS.getEntries().stream()
                 .map(RegistryObject::get).forEach(block -> {
             final BlockItem blockItem = new BlockItem(block, properties);
-            blockItem.setRegistryName(block.getRegistryName());
-            itemRegistry.register(blockItem);
+            if (block.getRegistryName() != null) {
+                blockItem.setRegistryName(block.getRegistryName());
+                itemRegistry.register(blockItem);
+            }
         });
 
     }
