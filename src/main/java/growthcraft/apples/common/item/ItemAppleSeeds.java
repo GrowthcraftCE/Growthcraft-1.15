af0cfd85e0b0e6a73c436fe8b19c15d2acd30a00
Alatyami <alatyami@gmail.com>
2020-05-12 18:46:53 -0400

    Re-implemented apple seeds
    In previous versions we dropped apple seeds. After the apple sapling/
    crop is finished, activating apple seeds on dirt will plant an apple
    sapling.

---
+++ src/main/java/growthcraft/apples/common/item/ItemAppleSeeds.java
@@ -0,0 +1,9 @@
+package growthcraft.apples.common.item;
+
+import growthcraft.core.shared.item.GrowthcraftItem;
+
+public class ItemAppleSeeds extends GrowthcraftItem {
+    public ItemAppleSeeds(String unlocalizedName) {
+        super(unlocalizedName);
+    }
+}
