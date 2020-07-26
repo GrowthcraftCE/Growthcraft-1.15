067d2fb37aac8e13585d7ffb42760ae0b46645ef
Alatyami <alatyami@gmail.com>
2020-07-26 15:43:26 -0400

    Progress #13 Implementation of Fishtraps
    Fishtraps can now be water logged with water.

--- src/main/java/growthcraft/trapper/lib/common/block/FishtrapBlock.java
+++ src/main/java/growthcraft/trapper/lib/common/block/FishtrapBlock.java
@@ -5,11 +5,17 @@ import growthcraft.trapper.init.GrowthcraftTrapperTileEntities;
 import growthcraft.trapper.lib.common.tileentity.TileEntityFishtrap;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -22,7 +28,9 @@ import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
-public class FishtrapBlock extends Block {
+public class FishtrapBlock extends Block implements IWaterLoggable {
+
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public FishtrapBlock() {
         this(getInitProperties(Material.WOOD));
@@ -30,6 +38,7 @@ public class FishtrapBlock extends Block {
 
     public FishtrapBlock(Properties properties) {
         super(properties);
+        this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, Boolean.valueOf(false)));
     }
 
     private static Properties getInitProperties(Material material) {
@@ -77,5 +86,13 @@ public class FishtrapBlock extends Block {
                 InventoryHelper.dropItems(worldIn, pos, ((TileEntityFishtrap) tile).getItems());
             }
         }
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+
+    public IFluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
     }
 }
